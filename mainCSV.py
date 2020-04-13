619385e67d64220f5c28522ed7bbba0acfddb4be
Matthew McCall <m.mccall88@gmail.com>
2020-04-13 09:52:44 -0400

    Minor edits for neatness.

--- mainCSV.py
+++ mainCSV.py
@@ -1,7 +1,24 @@
-# This file handles CSV files.
-# import csv, a library for handling CSV files.
+# This is the Main file for the program
+import smtplib
 import csv
-with open('MOCK.csv') as csv_file:
-    reader = csv.reader(csv_file)
-    for row in reader:
-        print(row)
+
+sender_gmail = 'vaclaims.us@gmail.com'
+sender_password = '123qwe!@#QWE'
+recipient_gmail = ['m.mccall88@gmail.com']
+body = 'Email sent successfully.'
+
+try:
+    server_ssl = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    server_ssl.login(sender_gmail, sender_password)
+    server_ssl.sendmail(sender_gmail, recipient_gmail, body)
+    server_ssl.close()
+    print 'Email sent!'
+
+except:
+    print 'Something went wrong...'
+
+# this section is for handling CSV files
+# with open('MOCK.csv') as csv_file:
+#     reader = csv.reader(csv_file)
+#     for row in reader:
+#         print(row)
--- mainSMTP.py
+++ mainSMTP.py
@@ -1,29 +1,23 @@
-# This file sends an email through gmail's SMTP server.
-# inputs: gmail account credentials, a recipient account, a subject and body.
-
-# import smtplib: a native python library for sending emails.
-import smtplib
-
-# define variables to store the gmail account credentials.
-gmail_user = 'GMAIL ACCOUNT'
-gmail_password = 'GMAIL ACCOUNT PASSWORD'
-
-# define variables to be parameters for the sendmail object.
-sent_from = gmail_user
-to = ['RECIPIENT GMAIL ACCOUNT']
-subject = 'Important Message!'
-body = 'Hey there, this is the email body.'
-
-# access gmail's SMTP server with Secure Socket Layer (SSL) encryption.
-# pass above variables to the login function and sendmail function.
-try:
-    server_ssl = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-    server_ssl.ehlo()
-    server_ssl.login(gmail_user, gmail_password)
-    server_ssl.sendmail(sent_from, to, body)
-    server_ssl.close
-
-# print success or error message to the console.
-    print 'Email sent!'
-except:
-    print 'Something went wrong...'
+# import smtplib
+#
+# gmail_user = 'vaclaims.us@gmail.com'
+# gmail_password = '123qwe!@#QWE'
+#
+# sent_from = gmail_user
+# to = ['mklekotko@gmail.com']
+# subject = 'Important Message!'
+# body = 'Hey there, this is the email body.'
+#
+# # access gmail's SMTP server with Secure Socket Layer (SSL) encryption.
+# # pass above variables to the login function and sendmail function.
+# try:
+#     server_ssl = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+#     server_ssl.ehlo()
+#     server_ssl.login(gmail_user, gmail_password)
+#     server_ssl.sendmail(sent_from, to, body)
+#     server_ssl.close()
+#
+# # print success or error message to the console.
+#     print 'Email sent!'
+# except:
+#     print 'Something went wrong...'
