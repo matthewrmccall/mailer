ea742ff9fcffc7bf629a75fd084628a395e22b42
Matthew McCall <m.mccall88@gmail.com>
2020-04-13 17:53:08 -0400

    Moved try/except block into the for row in header loop.

--- mainCSV.py
+++ mainCSV.py
@@ -15,14 +15,16 @@ with open('MOCK.csv') as csv_file:
     for row in reader:
         if len(row[2]) > 1:
             recipient_gmail = row[2]
+            try:
+                server_ssl = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+                server_ssl.login(sender_gmail, sender_password)
+                server_ssl.sendmail(sender_gmail, recipient_gmail, body)
+                server_ssl.close()
+                print 'Email sent!'
 
-try:
-    server_ssl = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-    server_ssl.login(sender_gmail, sender_password)
-    server_ssl.sendmail(sender_gmail, recipient_gmail, body)
-    server_ssl.close()
-    print 'Email sent!'
+            except:
+                print 'Something went wrong...'
 
-except:
-    print 'Something went wrong...'
+
+'
 
