ac56cefd82bd89fbde0b6505a37f77e5103c75eb
Matthew McCall <m.mccall88@gmail.com>
2020-04-13 10:12:03 -0400

    Minor edits for neatness.

--- mainCSV.py
+++ mainCSV.py
@@ -1,11 +1,17 @@
-# This is the Main file for the program
+# # This is the Main file for the program
 import smtplib
 import csv
 
 sender_gmail = 'vaclaims.us@gmail.com'
 sender_password = '123qwe!@#QWE'
-recipient_gmail = ['m.mccall88@gmail.com']
 body = 'Email sent successfully.'
+
+# this section is for handling CSV files
+with open('MOCK.csv') as csv_file:
+    reader = csv.reader(csv_file)
+    for row in reader:
+        if len(row[2]) > 1:
+            recipient_gmail = row[2]
 
 try:
     server_ssl = smtplib.SMTP_SSL('smtp.gmail.com', 465)
@@ -17,8 +23,3 @@ try:
 except:
     print 'Something went wrong...'
 
-# this section is for handling CSV files
-# with open('MOCK.csv') as csv_file:
-#     reader = csv.reader(csv_file)
-#     for row in reader:
-#         print(row)
